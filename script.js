6159dea4e1229bf9db5e93d553f64d67e0733bf0
Bennour Mehdi <bennourmehdi@macbook-air-de-bennour-1.home>
2026-04-23 13:25:49 +0200

    Interaction JavaScript sur les deux boutons

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Bravo Mehdi, ton environnement JavaScript est prêt !");
+// On attrape le bouton LinkedIn
+const linkedin = document.getElementById("btnLinkedin");
+linkedin.addEventListener("click", function() {
+    document.querySelector(".card").style.borderColor = "#0077b5"; // Bleu LinkedIn
+    console.log("Clic sur LinkedIn");
+});
+
+// On attrape le bouton GitHub
+const github = document.getElementById("btnGithub");
+github.addEventListener("click", function() {
+    document.querySelector(".card").style.borderColor = "#333"; // Gris foncé GitHub
+    console.log("Clic sur GitHub");
+});
