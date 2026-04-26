040c19e310a1759c522ba59ca046cd90b53a551a
Bennour Mehdi <bennourmehdi@macbook-air-de-bennour-1.home>
2026-04-26 19:10:04 +0200

    Ajout dark mode

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const github = document.getElementById("btnGithub");
 github.addEventListener("click", function() {
     document.querySelector(".card").style.borderTopColor = "#333"; // Noir GitHub
 });
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    const icon = themeToggle.querySelector("i");
+    if (document.body.classList.contains("dark-mode")) {
+        icon.classList.replace("fa-moon", "fa-sun");
+    } else {
+        icon.classList.replace("fa-sun", "fa-moon");
+    }
+});
