f033c3ff8086deaeb717ad688a73b3cdd3167ef5
Bennour Mehdi <bennourmehdi@macbook-air-de-bennour-1.home>
2026-04-26 19:17:05 +0200

    mémoire mode nuit

--- script.js
+++ script.js
@@ -8,14 +8,26 @@ github.addEventListener("click", function() {
     document.querySelector(".card").style.borderTopColor = "#333"; // Noir GitHub
 });
 const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
 
+// --- 1. AU CHARGEMENT : On vérifie s'il y a une note dans la mémoire ---
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    body.classList.add("dark-mode");
+    themeToggle.querySelector("i").classList.replace("fa-moon", "fa-sun");
+}
+
+// --- 2. AU CLIC : On change le thème ET on enregistre le choix ---
 themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    
+    body.classList.toggle("dark-mode");
     const icon = themeToggle.querySelector("i");
-    if (document.body.classList.contains("dark-mode")) {
+    
+    if (body.classList.contains("dark-mode")) {
         icon.classList.replace("fa-moon", "fa-sun");
+        localStorage.setItem("theme", "dark"); // On écrit "dark" dans le tiroir
     } else {
         icon.classList.replace("fa-sun", "fa-moon");
+        localStorage.setItem("theme", "light"); // On écrit "light" dans le tiroir
     }
 });
